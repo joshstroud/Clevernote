25bc9f63ac37c4f9d3506090536e90137553ff33
Josh Stroud <josh.e.stroud@gmail.com>
2018-12-11 15:09:42 -0800

    Implement Root and App containers for main app content

--- clevernote/frontend/clevernote.jsx
+++ clevernote/frontend/clevernote.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { testUserAuth } from "./tests/test_user_auth";
 import configureStore from "./store/store";
+import Root from "./components/root";
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
 
   testUserAuth(store);
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welcome to Clevernote</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
