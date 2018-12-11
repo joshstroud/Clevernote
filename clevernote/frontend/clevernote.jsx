53286a26dab1ce75f7332dc767eb5f7ca4adaa03
Josh Stroud <josh.e.stroud@gmail.com>
2018-12-11 15:04:28 -0800

    Implement and test full user authentication redux cycle

--- clevernote/frontend/clevernote.jsx
+++ clevernote/frontend/clevernote.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { testUserAuth } from "./tests/test_user_auth";
+import configureStore from "./store/store";
 
 document.addEventListener("DOMContentLoaded", () => {
+  const store = configureStore();
+
+  testUserAuth(store);
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to Clevernote</h1>, root);
 });
--- clevernote/frontend/tests/test_user_auth.js
+++ clevernote/frontend/tests/test_user_auth.js
@@ -1,17 +1,33 @@
+import * as SessionApiUtil from "../util/session_api_util"
+
 import {
   login,
   signup,
   logout
-} from "../util/session_api_util"
+} from "../actions/session_actions"
 
-export const testUserAuth = () => {
-  testSessionApiUtil();
+export const testUserAuth = (store) => {
+  // testSessionApiUtil();
+  testRedux();
+  setupStoreTesting(store);
+
 }
 
 const testSessionApiUtil = () => {
+  window.login = SessionApiUtil.login;
+  window.signup = SessionApiUtil.signup;
+  window.logout = SessionApiUtil.logout;
+}
+
+const testRedux = () => {
   window.login = login;
   window.signup = signup;
   window.logout = logout;
+}
+
+const setupStoreTesting = (store) => {
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 }
 
 const user = {
