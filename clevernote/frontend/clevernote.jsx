1d84d58f1f27b41f2920007e11c17e9a9d14f047
Josh Stroud <josh.e.stroud@gmail.com>
2018-12-11 17:18:55 -0800

    Refactor to login in without a page load, when user is already logged in

--- clevernote/frontend/clevernote.jsx
+++ clevernote/frontend/clevernote.jsx
@@ -5,9 +5,28 @@ import configureStore from "./store/store";
 import Root from "./components/root";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const store = configureStore();
+  const store = setupStoreWithUserLogin();
 
   testUserAuth(store);
   const root = document.getElementById("root");
   ReactDOM.render(<Root store={store} />, root);
 });
+
+const setupStoreWithUserLogin = () => {
+  let store = null;
+
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+
+  return store;
+};
