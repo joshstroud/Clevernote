376774a170396af31cb86ca1a602a0e0703d7fa0
Josh Stroud <josh.e.stroud@gmail.com>
2018-12-12 13:41:48 -0800

    Add test methods for AJAX calls

--- frontend/clevernote.jsx
+++ frontend/clevernote.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { testUserAuth } from "./tests/test_user_auth";
+// import { testUserAuth } from "./tests/test_user_auth";
+import { testNotes } from "./tests/test_notes";
 import configureStore from "./store/store";
 import Root from "./components/root";
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = setupStoreWithUserLogin();
 
-  testUserAuth(store);
+  testNotes(store);
   const root = document.getElementById("root");
   ReactDOM.render(<Root store={store} />, root);
 });
--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -22,7 +22,6 @@ class SessionForm extends React.Component {
       email: this.state.email,
       password: this.state.password
     };
-    debugger;
     this.props.processForm(user);
   }
 
