ac4035c3b1385ea9f832a862f37e1c19e10b22ba
Josh Stroud <josh.e.stroud@gmail.com>
2018-12-12 13:43:58 -0800

    Add notes reducer and add to parent entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -3,7 +3,9 @@ import {
 } from 'redux';
 
 import users from './users_reducer';
+import notes from './notes_reducer';
 
 export default combineReducers({
   users,
+  notes
 });
---
+++ frontend/reducers/notes_reducer.js
@@ -0,0 +1,27 @@
+import merge from 'lodash/merge';
+
+import {
+  RECEIVE_ALL_NOTES,
+  RECEIVE_SINGLE_NOTE,
+  DELETE_NOTE,
+} from '../actions/note_actions';
+
+const notesReducer = (state = {}, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_ALL_NOTES:
+      return action.notes;
+    case RECEIVE_SINGLE_NOTE:
+      return merge({}, state, {
+        [action.note.id]: action.note
+      })
+    case DELETE_NOTE:
+      let newState = merge({}, state);
+      delete newState[action.noteId];
+      return newState
+    default:
+      return state;
+  }
+};
+
+export default notesReducer;
