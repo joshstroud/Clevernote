5a168b3693c9627e9b483a19043468cc3d494546
Josh Stroud <josh.e.stroud@gmail.com>
2018-12-11 17:43:41 -0800

    Add placeholder greeting to signal end of login flow, add route to greeting when logged in

--- clevernote/frontend/components/app.jsx
+++ clevernote/frontend/components/app.jsx
@@ -3,14 +3,16 @@ import LoginGreetingContainer from "./test/login_greeting_container";
 import LoginFormContainer from "./session/login_form_container";
 import SignupFormContainer from "./session/signup_form_container";
 import { Route } from "react-router-dom";
-import { AuthRoute } from "../util/route_util";
+import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import SplashContainer from "./splash/splash_container";
+import MainPage from "./main_page/main_page";
 
 const App = () => (
   <div>
     <Route exact path="/" component={SplashContainer} />
     <AuthRoute path="/login" component={LoginFormContainer} />
     <AuthRoute path="/signup" component={SignupFormContainer} />
+    <ProtectedRoute path="/app" component={MainPage} />
   </div>
 );
 
---
+++ clevernote/frontend/components/main_page/main_page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginGreetingContainer from "../test/login_greeting_container";
+
+const MainApp = () => {
+  return (
+    <main>
+      <LoginGreetingContainer />
+    </main>
+  );
+};
+
+export default MainApp;
