9da0be5caa35238028498f7ad889c242fd224685
Josh Stroud <josh.e.stroud@gmail.com>
2018-12-13 10:51:28 -0800

    Finish HTML and CSS for splash header

--- frontend/components/splash/splash_header.jsx
+++ frontend/components/splash/splash_header.jsx
@@ -4,26 +4,28 @@ import { Link } from "react-router-dom";
 const SplashHeader = props => {
   return (
     <header className="global-header">
-      <Link to="/">
-        <img
-          className="global-logo"
-          src={window.images.homeClevernoteLogo}
-          alt="Evernote logo"
-        />
-      </Link>
-      <nav className="utility-nav">
-        <ul>
-          <li>
-            <Link to="/signup">Sign up</Link>
-          </li>
-          <li>or</li>
-          <li>
-            <Link to="/login" className="button-outline">
-              Log in
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <div className="global-header-wrapper">
+        <Link to="/">
+          <img
+            className="global-logo"
+            src={window.images.homeClevernoteLogo}
+            alt="Evernote logo"
+          />
+        </Link>
+        <nav className="utility-nav">
+          <ul>
+            <li>
+              <Link to="/signup">Sign up</Link>
+            </li>
+            <li>or</li>
+            <li>
+              <Link to="/login" className="button-outline">
+                Log in
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
