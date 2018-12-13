ca09fbca37b418173382f29189c4a77a626c8c17
Josh Stroud <josh.e.stroud@gmail.com>
2018-12-13 10:51:53 -0800

    Finish HTML and CSS for spash hero

--- frontend/components/splash/splash_hero.jsx
+++ frontend/components/splash/splash_hero.jsx
@@ -3,19 +3,21 @@ import { Link } from "react-router-dom";
 
 const SplashHero = props => {
   return (
-    <section className="hero top-sign-up green pattern-background">
-      <div className="content">
-        <h1>Feel organized without the effort</h1>
-        <div className="description">
-          Evernote helps you capture and prioritize ideas, projects, and to-do
-          lists, so nothing falls through the cracks.
+    <main className="splash-content-wrapper">
+      <section className="hero top-sign-up green pattern-background">
+        <div className="hero-content">
+          <h1>Feel organized without the effort</h1>
+          <div className="description">
+            Evernote helps you capture and prioritize ideas, projects, and to-do
+            lists, so nothing falls through the cracks.
+          </div>
+          <Link to="/signup" className="button-primary white">
+            Sign up for free
+          </Link>
         </div>
-        <Link to="/signup" className="button-primary white">
-          Sign up for free
-        </Link>
-      </div>
-      <img src={window.images.homepageHero} alt />
-    </section>
+        <img src={window.images.homepageHero} className="hero-image" />
+      </section>
+    </main>
   );
 };
 
